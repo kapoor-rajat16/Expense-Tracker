f18bb05fc1afa282b137506704f5ba456e3c275c
rajat kapoor <rajatkapoor214@gmail.com>
2021-10-09 22:48:16 +0530

    integrate mongoose

--- app.js
+++ app.js
@@ -1,33 +1,72 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
+mongoose.connect("mongodb://localhost:27017/etUserDB", { useNewUrlParser: true });
 
-app.get("/",function (req,res) {
+const userSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+    password: String
+});
+
+const User = mongoose.model("User",userSchema);
+
+const user = new User({
+    username: "firstUser",
+    email: "firstUser@xyz.com",
+    password: "first"
+});
+
+// user.save();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
     res.sendFile(__dirname + "/home.html");
 });
 
-app.get("/login", function (req,res) {
+app.get("/login", function (req, res) {
     res.sendFile(__dirname + "/login.html");
 });
 
-app.get("/signup", function (req,res) {
+app.get("/signup", function (req, res) {
     res.sendFile(__dirname + "/signup.html");
 });
 
-// app.post("/", function (req,res){
-//     console.log(req.body);
-//     if (req.body.name=="login") {
-//         res.redirect("login");
-//     }
-//     else{
-//         res.redirect("signup");
-//     }
-// });
+app.get("/tracker",function (req,res) {
+    res.sendFile(__dirname + "/tracker.html");
+});
 
-app.listen(3000,function () {
+app.post("/signup", function (req, res) {
+
+    const newUser = new User({
+        username:req.body.username,
+        email:req.body.email,
+        password:req.body.password
+    });
+
+    newUser.save(function(err){
+        if(err){
+            console.log(err);
+            res.render("login");
+        }
+        else{
+            res.render("tracker");
+        }
+    });
+});
+
+
+app.post("/login", function (req, res) {
+    console.log(req.body.username);
+    console.log(req.body.password);
+});
+
+
+app.listen(3000, function () {
     console.log("Server is running on port 3000");
 });
